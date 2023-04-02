7d747cf9d23a7235138cbb29e6ad330767ef7149
Itay Navon <83724543+ItayN137@users.noreply.github.com>
2023-04-02 12:03:49 +0300

    Update tryserver.py

--- tryserver.py
+++ tryserver.py
@@ -4,47 +4,41 @@ from PIL import ImageGrab
 import io
 import time
 
+
 class Server:
+
+    def open_udp_server(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind(self.server_address)
 
     def handle_data(self, s):
         """Function to handle the data from client connection and send it back"""
         while True:
-            # Recieve the data from the client
-            self.length, self.client_address = s.recvfrom(65000)
-            self.data, self.client_address = s.recvfrom(int(self.length.decode()))
+            # Receive the data from the client
+            length, client_address = s.recvfrom(65000)
+            data, client_address = s.recvfrom(int(length.decode()))
 
             # Send back the screenshot
-            self.s.sendto(self.length, self.client_address)
-            self.s.sendto(self.data, self.client_address)
+            self.server_socket.sendto(length, client_address)
+            self.server_socket.sendto(data, client_address)
 
     def start(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        self.s.bind(self.server)
-
-        t = threading.Thread(target=self.handle_data, args=(self.s,))
+        t = threading.Thread(target=self.handle_data, args=(self.server_socket,))
         t.start()
-
-        # Close the socket
-        #self.s.close()
 
     def __init__(self):
         # Create a socket object
-        self.s = 0
+        self.server_socket = None
 
         # Bind the socket to a specific host and port
         self.host = socket.gethostname()
         self.port = 12345
-        self.server = (self.host, self.port)
-
-
-
-        # Start a new thread to send the data
-
+        self.server_address = (self.host, self.port)
 
 
 def main():
     s = Server()
+    s.open_udp_server()
     s.start()
 
 
