29698acffb838baa90cdd633b348cbebcf4dc251
Itay Navon <83724543+ItayN137@users.noreply.github.com>
2023-04-04 16:56:17 +0300

    Update tryserver.py

--- tryserver.py
+++ tryserver.py
@@ -1,11 +1,24 @@
 import socket
 import threading
+from multiprocessing import pool
+
 from PIL import ImageGrab
 import io
 import time
+import pyaudio
+import concurrent.futures
 
 
-class Server:
+class StreamingServer:
+
+    def __init__(self):
+        # Create a socket object
+        self.server_socket = None
+
+        # Bind the socket to a specific host and port
+        self.host = socket.gethostname()
+        self.port = 12345
+        self.server_address = (self.host, self.port)
 
     def open_udp_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,7 +39,15 @@ class Server:
         t = threading.Thread(target=self.handle_data, args=(self.server_socket,))
         t.start()
 
+
+class AudioServer:
+
     def __init__(self):
+        self.__chunk = 1024
+        self.__format = pyaudio.paInt16
+        self.__channels = 1
+        self.__rate = 44100
+
         # Create a socket object
         self.server_socket = None
 
@@ -35,9 +56,28 @@ class Server:
         self.port = 12345
         self.server_address = (self.host, self.port)
 
+        # Open to udp server
+        self.open_udp_server()
+
+        # Create a thread pool with a limited number of workers
+        self.pool = concurrent.futures.ThreadPoolExecutor(max_workers=8)
+
+    def open_udp_server(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind(self.server_address)
+
+
+
+    def handle_data(self):
+        while True:
+            # Receive a chunk of audio data from a client
+            data, address = self.server_socket.recvfrom(4096)
+
+            # Send the data back to Clients
+            self.server_socket.sendto(data, address)
 
 def main():
-    s = Server()
+    s = StreamingServer()
     s.open_udp_server()
     s.start()
 
